bbcf731a1bbf7accd1153565b72ce5a8f2177a67
kennywindis <kennyphilip100@gmail.com>
2023-04-18 00:17:51 -0700

    basic

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,9 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
   const k=[];
-  for (var idx of array) {
+  for (const idx of array) {
     const value = idx;
     k.push(appendString + value);
   }
 
-  return array;
+  return k;
 }
