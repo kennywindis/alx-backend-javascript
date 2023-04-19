740990b82f0d36e2ccc491d2e11429c16729c44c
kennywindis <kennyphilip100@gmail.com>
2023-04-19 05:44:30 -0700

    promise

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,12 +1,12 @@
 export default function getFullResponseFromAPI(success) {
-let k = new Promise (function (resolve, reject) {
-if (success === true) {
-	resolve({status: 200,
-		 body: 'Success',});
-}
-else {
-	reject( d error('The fake API is not working currently'));
-}
-})
-return k;
+  return new Promise((resolve, reject) => {
+    if (success === true) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
 }
