08f469bd80dcb835448ff0989732be3ccf5a5d3a
kennywindis <kennyphilip100@gmail.com>
2023-04-18 19:40:52 -0700

    promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,11 +1,3 @@
 export default function getResponseFromAPI() {
-const k = new Promise(function (resolve, reject) {
-	if (k >= 0) {
-		resolve (k)
-	}
-	else{
-	reject('not')
-})
-	return k;
-}
+	return new Promise(function (resolve, reject) {});
 
