4ffc333985bc3bd167290f5fae5e70f57fcc3897
kennywindis <kennyphilip100@gmail.com>
2023-04-18 18:55:13 -0700

    promise

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,15 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((values) => {
+    const k = [];
+    for (const item of values) {
+      k.push({ status: item.status, value: item.value || item.reason });
+    }
+    return arr;
+  });
+}
