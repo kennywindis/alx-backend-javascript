6f471f701093be5ad412203b62a25022adb69cdc
kennywindis <kennyphilip100@gmail.com>
2023-04-19 14:20:32 -0700

    promise

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,12 +1,12 @@
 export default function guardrail(mathFunction) {
   const queue = [];
-  const k;
+  let value;
   try {
     k = mathFunction();
   } catch (error) {
     k = error.toString();
   }
-queue.push(k);
+  queue.push(k);
   queue.push('Guardrail was processed');
   return queue;
 }
