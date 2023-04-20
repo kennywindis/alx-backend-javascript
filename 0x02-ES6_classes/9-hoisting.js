6b63c193b205d860aa7bbae2cd682f3bd5abfc20
kennywindis <kennyphilip100@gmail.com>
2023-04-20 11:00:20 -0700

    class

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -17,18 +17,18 @@ const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
 export class StudentHolberton {
-  constructor(firstName, lastName) {
+  constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
     this._lastName = lastName;
     this._holbertonClass = holbertonClass;
   }
 
-get fullName() {
+  get fullName() {
     return `${this._firstName} ${this._lastName}`;
   }
 
   get holbertonClass() {
-    return this.holbertonClass;
+    return this._holbertonClass;
   }
 }
 
