bea03575b40c3b386e2a5358b956c60fb76c7f72
kennywindis <kennyphilip100@gmail.com>
2023-04-25 02:54:39 -0700

    dm

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+export default function getListStudents() {
+  return [
+{ firstName: 'Guillame', id: 1, location: 'San Francisco' },
+{ firstName: 'James', id: 2, location: 'Columbia' },
+{ firstName: 'Serena', id: 5, location: 'San Francisco' },
+  ];
+}
