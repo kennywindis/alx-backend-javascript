7fab67c06edcb8d78da13a7ea4edff592bc4e8e1
kennywindis <kennyphilip100@gmail.com>
2023-04-25 03:10:14 -0700

    dm

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,11 @@
+export default function getStudentsByLocation(array, city, grad) {
+  return array
+    .filter((k) => k.location === city)
+    .map((student) => {
+      const gradeI = grad
+        .filter((k) => i.studentId === student.id)
+        .map((x) => x.grade)[0];
+      const grade = gradeI || 'N/A';
+      return { ...student, grade };
+    });
+}
