f98a35f6166919c7237cd8c5a087aef05f07b3d1
kennywindis <kennyphilip100@gmail.com>
2023-06-14 00:08:50 -0700

    node

--- 0x05-Node_JS_basic/full_server/server.js
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -1,5 +1,12 @@
-const app = require('express')();
+import express from 'express';
 
-app.listen(1245);
+const app = express();
+const indexRouter = require('./routes/index');
+const port = 1245;
+
+app.listen(port);
+app.use('/', indexRouter);
+app.use('/students', indexRouter);
+app.use('/students/:major', indexRouter);
 
 export default app;
