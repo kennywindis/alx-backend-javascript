f77a3c8d043e6558ba4ca80c2f389c004c4ab356
kennywindis <kennyphilip100@gmail.com>
2023-06-13 23:53:56 -0700

    node

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,9 +1,18 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+#!/usr/bin/node
+
+const process = require('process');
+
 process.stdin.setEncoding('utf8');
+
+console.log('Welcome to Holberton School, what is your name?');
+
 process.stdin.on('readable', () => {
-  const name = process.stdin.read();
-  if (name) process.stdout.write(`Your name is: ${string}`);
+  const chunk = process.stdin.read();
+  if (chunk !== null) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
 });
+
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
