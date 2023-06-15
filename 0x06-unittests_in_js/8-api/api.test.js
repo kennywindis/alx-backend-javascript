dbde8b15069b3b30d0b5f006d2153e4da6497462
kennywindis <kennyphilip100@gmail.com>
2023-06-15 03:34:58 -0700

    ujs

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,13 +1,19 @@
-const { expect } = require("chai");
 const request = require('request');
+const { expect } = require('chai');
 
-describe('Index page', () => {
-    describe('GET /', () => {
-        it('checks output of GET /', (done) => {
-            request('http://localhost:7865', (err, res, body) => {
-                expect(res.statusCode).to.equal(200);
-                expect(body).to.equal('Welcome to the payment system');
-            }, done())
-        })
-    })
-})
+const url = 'http://localhost:7865';
+
+describe('Express app', () => {
+  it('Test code status', (done) => {
+    request.get(url, function (err, res, body) {
+      expect(res.statusCode).to.equal(200);
+      done()
+    });
+  });
+  it('output', (done) => {
+    request.get(url, function (err, res, body) {
+      expect(body).to.equal('Welcome to the payment system');
+      done()
+    });
+  });
+});
