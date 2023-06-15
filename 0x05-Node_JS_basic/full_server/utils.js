676e5542971512e1e94c56b2f9d22eab45994c5f
kennywindis <kennyphilip100@gmail.com>
2023-06-15 04:07:34 -0700

    node

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,30 +1,31 @@
 const fs = require('fs');
 
 async function readDatabase(path) {
-    if (!fs.existsSync(path)) {
-        throw Error('Cannot load the database');
-    }
-    const data = await fs.promises.readFile(path, 'utf-8');
-    const students = data.split('\r\n').slice(1)
-    .map((student) => student.split(','))
-    .map((student) => ({
-      firstName: student[0],
-      lastName: student[1],
-      age: student[2],
-      field: student[3],
-    }));
-    let fields = students.map(student => student.field);
-    let unique_fields = new Set(fields);
-    let students_by_field = {};
-    for (let field of unique_fields) {
-      students_by_field[field] = [];
-    }
-    for (let student of students) {
-        students_by_field[student.field].push(student.firstName);
-    }
-    console.log(students_by_field);
-    return students_by_field;
-    // console.log(students_by_field);
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, { encoding: 'utf8' }, (err, data) => {
+      if (err) {
+        reject(Error('Cannot load the database'));
+        return;
+      }
+      const resp = {};
+
+      let content = data.toString().split('\n');
+      content = content.filter((item) => item);
+      if (content.length > 0) {
+        content.shift();
+      }
+
+      const students = content.map((student) => student.split(','));
+      const setField = new Set();
+      students.forEach((student) => setField.add(student[3]));
+      setField.forEach((field) => {
+        const group = students.filter((student) => student[3] === field);
+        const names = group.map((student) => student[0]);
+        resp[field] = names;
+      });
+      resolve(resp);
+    });
+  });
 }
 
 module.exports = readDatabase;
