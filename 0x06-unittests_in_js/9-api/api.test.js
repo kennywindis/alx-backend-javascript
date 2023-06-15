54bb5408e88a30c52b681a0c8e08f23866a8d97d
kennywindis <kennyphilip100@gmail.com>
2023-06-15 03:09:15 -0700

    ujs

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -1,38 +1,40 @@
-const { expect } = require("chai");
 const request = require('request');
+const { expect } = require('chai');
+
 const url = 'http://localhost:7865';
 
-describe('Index page', () => {
-    it('GET / exists and outputs', (done) => {
-        request(url, (err, res, body) => {
-            expect(res.statusCode).to.equal(200);
-            expect(body).to.equal('Welcome to the payment system');
-            done();
-        });
+describe('Express app /', () => {
+  it('/ ok code status', (done) => {
+    request.get(url, function (err, res, body) {
+      expect(res.statusCode).to.equal(200);
+      done()
     });
+  });
+  it('/ correct output', (done) => {
+    request.get(url, function (err, res, body) {
+      expect(body).to.equal('Welcome to the payment system');
+      done()
+    });
+  });
 
-    it('GET /cart/:id', (done) => {
-        request(`${url}/cart/12`, (err, res, body) => {
-            expect(res.statusCode).to.equal(200);
-            expect(body).to.equal('Payment methods for cart 12');
-            done();
-        });
+  describe('Express app /cart/:id', () => {
+    it('ok code status', (done) => {
+      request.get(`${url}/cart/5`, (error, response, body) => {
+        expect(response.statusCode).to.equal(200);
+        done()
+      });
     });
-
-    it('GET /cart/:id with id hello', (done) => {
-        request(`${url}/cart/hello`, (err, res, body) => {
-            expect(res.statusCode).to.equal(404);
-            expect(body).to.equal('<!DOCTYPE html>\n' +
-            '<html lang="en">\n' +
-            '<head>\n' +
-            '<meta charset="utf-8">\n' +
-            '<title>Error</title>\n' +
-            '</head>\n' +
-            '<body>\n' +
-            '<pre>Cannot GET /cart/hello</pre>\n' +
-            '</body>\n' +
-            '</html>\n');
-            done();
-        });
+    it('correct output', (done) => {
+      request.get(`${url}/cart/5`, (error, response, body) => {
+        expect(body).to.equal('Payment methods for cart 5');
+        done()
+      });
     });
+    it('status code 404', (done) => {
+      request.get(`${url}/cart/asd`, (error, response, body) => {
+        expect(response.statusCode).to.equal(404);
+        done()
+      });
+    });
+  });
 });
