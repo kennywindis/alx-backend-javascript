5f5b290ba0f789a43c9f454674b927ec6a63166e
kennywindis <kennyphilip100@gmail.com>
2023-06-15 03:00:28 -0700

    ujs

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -1,19 +1,18 @@
 const express = require('express');
 
 const app = express();
+const hostname = 'localhost';
 const port = 7865;
 
-app.listen(port, console.log(`API available on localhost port ${port}`));
-app.use(express.json())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
   res.send('Welcome to the payment system');
 });
 
 app.get('/cart/:id([0-9]+)', (req, res) => {
-  console.log(`${req.params.id}`)
-  res.end(`Payment methods for cart ${req.params.id}`);
+  res.send(`Payment methods for cart ${req.params.id}`)
 });
 
 app.get('/available_payments', (req, res) => {
@@ -22,8 +21,16 @@ app.get('/available_payments', (req, res) => {
       credit_cards: true,
       paypal: false
     }
-  })
+  });
 });
-app.post('/login', (req, res) => res.end(`Welcome ${req.body.userName}`));
+
+app.post('/login', (req, res) => {
+  // console.log(req.body);
+  res.send(`Welcome ${req.body.userName}`)
+});
+
+app.listen(port, () => {
+  console.log(`API available on ${hostname} port ${port}`);
+});
 
 module.exports = app;
