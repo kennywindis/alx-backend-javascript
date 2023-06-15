d20b46b75546f8a6f7dbf986af3c5c66871c8a38
kennywindis <kennyphilip100@gmail.com>
2023-06-15 03:33:26 -0700

    ujs

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -1,14 +1,15 @@
 const express = require('express');
 
 const app = express();
+const hostname = 'localhost';
 const port = 7865;
 
 app.get('/', (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
   res.send('Welcome to the payment system');
 });
 
-app.listen(port, console.log(`API available on localhost port ${port}`));
+app.listen(port, () => {
+  console.log(`API available on ${hostname} port ${port}`);
+});
 
-module.exports = [app, port];
+module.exports = app;
