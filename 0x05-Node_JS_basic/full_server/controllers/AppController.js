2a24e8c73ccfa8b5da52c07af2054e434dfa37c3
kennywindis <kennyphilip100@gmail.com>
2023-06-29 01:15:35 -0700

    node

--- 0x05-Node_JS_basic/full_server/controllers/AppController.js
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -1,6 +1,7 @@
-export default class AppController {
+class AppController {
   static getHomepage(request, response) {
-    response.status(200);
-    response.send('Hello Holberton School!');
+    response.send(200, 'Hello Holberton School!');
   }
 }
+
+export default AppController;
